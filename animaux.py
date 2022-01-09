b12f8d757ad758dcf31f94055085c0a2e6436b3d
doddyz <doddyz@live.com>
2022-01-09 10:02:05 +0000

    get_animals_data and draw_images_grid ready

--- animaux.py
+++ animaux.py
@@ -1,5 +1,8 @@
 # Next Dataframe function
 # Then scrape from Live Webpage any african country first
+# Remove unused vars & funs once main functions built
+# See if possible to remove past uncessary commits from version history
+# Translate BS manual into French + Spanish (Linguee)
 # Images et description par pays (afrique puis Europe)
 # On peut creer une librairie simple utilisant les fonctions de base (généralité de ce que lon compose) 
 
@@ -26,14 +29,16 @@ def get_page_local_soup(country):
 
 
 def get_page_soup(country):
-    r = requests.get(BASE_URL + country + '/')
+    continent = 'africa'
+    r = requests.get(BASE_URL + 'location/' + continent + '/' + country + '/')
     soup = BeautifulSoup(r.content, 'html.parser')
     return soup
 
 
-def get_animals_data_from_file():
+def get_animals_data_from_file(filename):
+    page_urls, image_urls, names, descriptions = [], [], [], []
     data = {}
-    with open('togo.html') as f:
+    with open(filename) as f:
         soup = BeautifulSoup(f.read(), 'html.parser')
         card_divs = soup.find_all('div', class_='card')
         for card_div in card_divs:
@@ -46,34 +51,50 @@ def get_animals_data_from_file():
             else:
                 description = description.get_text()
             
-            print(f'{name}, {page_url}, {image_url}, {description}')
-    
+            # print(f'{name}, {page_url}, {image_url}, {description}')
+            page_urls.append(page_url)
+            image_urls.append(image_url)
+            names.append(name)
+            descriptions.append(description)
 
+        data['page_urls'] = page_urls
+        data['image_urls'] = image_urls
+        data['names'] = names
+        data['descriptions'] = descriptions
+        
+    return pd.DataFrame(data)
+
+
+# @st.cache        
 def get_animals_data(country):
+    page_urls, image_urls, names, descriptions = [], [], [], []
     data = {}
+    
     soup = get_page_soup(country)
     card_divs = soup.find_all('div', class_='card')
-    for result in card_divs:
-        for child in result.children:
-            if isinstance(child, NavigableString):
-                continue
-            if isinstance(child, Tag):
-                page_url_tag = child.a
-                image_url_tag = child.img
-                print(f'{page_url_tag}, {image_url_tag}')
+    for card_div in card_divs:
+        page_url = card_div.a['href']
+        image_url = card_div.img['src']
+        name = card_div.h5.a.get_text()
+        description = card_div.find(class_='card-fun-fact')
+        if description is None:
+            description = 'Dummy empty description here'
+        else:
+            description = description.get_text()
                 
-                # page_url = page_url_tag['href']
-                # image_url = image_url_tag['src']
-        # name = card.img.div.div.h5.content
-        # description = DESCRIPTION_PATTERN.search(card).groups()[0]
-        
-            # data[page_url_tag.contents] = {'image_url': image_url_tag.contents}
+            # print(f'{name}, {page_url}, {image_url}, {description}')
+        page_urls.append(page_url)
+        image_urls.append(image_url)
+        names.append(name)
+        descriptions.append(description)
 
-    # return data
+    data['page_urls'] = page_urls
+    data['image_urls'] = image_urls
+    data['names'] = names
+    data['descriptions'] = descriptions
+        
+    return pd.DataFrame(data)
 
             
-        
-
-# @st.cache
-def df_from_content(country):
-    pass
+def draw_images_grid_from_df(df):
+    st.images(df['image_urls'].to_list(), df['Names'].to_list())
